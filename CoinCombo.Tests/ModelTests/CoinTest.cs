65722cd7163717b7f62210aac88773800bc167ca
Jim Palowski and Verna Santos <verna@>
2018-04-24 16:44:45 -0700

    Testing

---
+++ CoinCombo.Tests/ModelTests/CoinTest.cs
@@ -0,0 +1,5 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+
+using CoinCombo.Models;
---
+++ CoinCombo/Models/Coin.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System;
+
+namespace CoinCombo.Models
+{
+  public class Combo
+  {
+    [TestClass]
+    public class ComboTest
+    {
+    [TestMethod]
+    public void GetCoinCombination()
+    {
+
+    }
+    }
+  }
+}
